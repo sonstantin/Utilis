36312ad9413d19cdc47ecf516cd47868e8709092
sonstantin <165193495+sonstantin@users.noreply.github.com>
2026-07-16 15:29:57 +0200

    Add basic arithmetic functions in basic.py
    Implement basic arithmetic functions including addition, subtraction, multiplication, division, floor division, and modulo. Add a main block to demonstrate usage.

---
+++ basic.py
@@ -0,0 +1,56 @@
+def add(toadd:list):
+    output = 0
+    for num in toadd:
+        try:
+            output += num
+        except TypeError:
+            raise TypeError("Input has to be an Integer or a float")
+    return output
+def substract(first:float,tominus:list):
+    output = first
+    for num in tominus:
+        output -= num
+    return output
+def multiply(tomulti:list):
+    output = 1
+    for num in tomulti:
+        output = output * num
+    return output
+def devide(first:float, second:float):
+    return first / second
+def floor(first:float, second:float):
+    return first // second
+def modulo(first:float, second:float):
+    return first % second
+def devideFull(first:float, second:float):
+    return {"Devide": first / second,
+    "Floor": first // second,
+    "Modulo": first % second}
+    
+if __name__ == "__main__":
+    list = [1,2,3]
+    value = add(list)
+    print(value)
+    
+    integer = 12
+    value = substract(integer, list)
+    print(value)
+    
+    list = [1,2,3]
+    value = multiply(list)
+    print(value)
+    
+    first = 18
+    second = 3
+    
+    value = devide(first, second)
+    print(value)
+    
+    value = floor(first, second)
+    print(value)
+    
+    value = modulo(16, 10)
+    print(value)
+    
+    value = devideFull(first, second)
+    print(value)
