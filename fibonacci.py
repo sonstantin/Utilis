4fea64c795074c314a60d95b41d88d9ea1ba1c81
sonstantin <165193495+sonstantin@users.noreply.github.com>
2026-07-16 13:54:03 +0200

    Update fibonacci function to handle float inputs
    Changed input type from int to float and formatted output.

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,11 @@
-start = int(input("Gib eine Zahl ein: "))
+start = float(input("Gib eine Zahl ein: "))
 times = int(input("Gib die Zahl der Wiederholungen an: "))
 def fibonacci(start, times):
     first = start
     second = start
     for i in range(times):
         number = first + second
-        print(f"{i+1}) {first} + {second} = {number}")
+        print(f"{i+1}) {first:.2f} + {second:.2f} = {number:.2f}")
         first = second
         second = number
 fibonacci(start, times)
