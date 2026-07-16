8dbd74904456e5421f90188372cb749ae62ca472
sonstantin <165193495+sonstantin@users.noreply.github.com>
2026-07-16 13:10:34 +0200

    Refactor Fibonacci logic into a function

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,11 @@
 start = int(input("Gib eine Zahl ein: "))
 times = int(input("Gib die Zahl der Wiederholungen an: "))
-first = start
-second = start
-for i in range(times):
-    number = first + second
-    print(f"{i+1}) {first} + {second} = {number}")
-    first = second
-    second = number
+def fibonacci(start, times):
+    first = start
+    second = start
+    for i in range(times):
+        number = first + second
+        print(f"{i+1}) {first} + {second} = {number}")
+        first = second
+        second = number
+fibonacci(start, times)
