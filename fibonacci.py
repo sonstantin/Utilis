1d78aa8eec427658ca86176068698debf3c51583
sonstantin <165193495+sonstantin@users.noreply.github.com>
2026-07-16 14:14:43 +0200

    Add type hints and return list in fibonacci function
    Refactor fibonacci function to include type hints and return a list of Fibonacci numbers.

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,19 @@
-start = float(input("Gib eine Zahl ein: "))
-times = int(input("Gib die Zahl der Wiederholungen an: "))
-def fibonacci(start, times):
+def fibonacci(start: float, times:int):
     first = start
     second = start
+    list = []
     for i in range(times):
         number = first + second
         print(f"{i+1}) {first:.2f} + {second:.2f} = {number:.2f}")
+        list.append(number)
         first = second
         second = number
-fibonacci(start, times)
+    return list
+
+
+if __name__ == "__main__":
+    start = float(input("Gib eine Zahl ein: "))
+    times = int(input("Gib die Zahl der Wiederholungen an: "))
+    output = fibonacci(start, times)
+    print(output)
+    
