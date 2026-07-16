b879a325e834ace5116dec9c67ad2973f564b9b6
sonstantin <165193495+sonstantin@users.noreply.github.com>
2026-07-16 15:30:40 +0200

    Add Test.py for various mathematical operations

---
+++ Test.py
@@ -0,0 +1,25 @@
+import Utilis
+
+output = Utilis.Mathematics.fibonacci(5.0, 3)
+print(output)
+
+output = Utilis.Mathematics.basic.add([1,2,3])
+print(output)
+
+output = Utilis.Mathematics.basic.substract(12, [1,2,3])
+print(output)
+
+output = Utilis.Mathematics.basic.multiply([1,2,3])
+print(output)
+
+output = Utilis.Mathematics.basic.devide(18.0, 3)
+print(output)
+
+output = Utilis.Mathematics.basic.floor(18.0, 3)
+print(output)
+
+output = Utilis.Mathematics.basic.modulo(16.0, 10)
+print(output)
+
+output = Utilis.Mathematics.basic.devideFull(18.0, 3)
+print(output)
